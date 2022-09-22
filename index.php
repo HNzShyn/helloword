07825132dbb59f38d6c4baec4ac1441371751887
HNzShyn <nvu1343@gmail.com>
2022-09-22 16:26:37 +0700

    ok

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
     echo"<h1 style='color: aqua'>Hello Word</h1>";
     echo"<h2 style='color: green'>Uploading code to Heroku successfully!!!</h2>";
+    echo"<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/330px-Image_created_with_a_mobile_phone.png'>"
 ?>
